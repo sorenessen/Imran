a0713b8b733342ce3821d7054af8dfbd8a6ea554
soren <sorenessen@gmail.com>
2016-08-25 09:12:43 -0400

    fixed Before and After methods

--- Generic/src/main/java/common/Base.java
+++ Generic/src/main/java/common/Base.java
@@ -1,14 +1,24 @@
 package common;
-import org.testng.annotations.BeforeMethod;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Faheem on 8/24/2016.
+ * Created by soren on 8/24/2016.
  */
 public class Base {
     public WebDriver driver = null;
-
     @BeforeMethod
+    public void setUp(String url){
 
 
+    }
+
+
+    @AfterMethod
+    public void cleanUp(){
+
+    }
 }
